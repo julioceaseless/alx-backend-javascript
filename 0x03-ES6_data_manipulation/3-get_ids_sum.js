5504e53f3f898b9b0c6ad9ccc520ccf13f1871bf
julioceaseless <julioceaseless@gmail.com>
2024-07-03 05:24:21 +0300

    More tasks

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,14 @@
+/**
+ * Adds the student ids
+ * @returns sum of ids
+ */
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
+
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+/**
+ * Creates an array buffer with an Int8 value at a specific position
+ * @returns {DataView}
+ */
+
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
+}
